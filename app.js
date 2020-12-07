1b9f23479d7940e16eb1deac27c472ff11d211d0
dakshsethi <sethidaksh02@gmail.com>
2020-12-07 21:36:02 +0530

    Test1

---
+++ app.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDAIX8Fb8_7D10DGV3516WWwvIvvx3R7u4",
+    authDomain: "to-do-ee845.firebaseapp.com",
+    projectId: "to-do-ee845",
+    storageBucket: "to-do-ee845.appspot.com",
+    messagingSenderId: "1062394243290",
+    appId: "1:1062394243290:web:ca94e8f822a73bedae5de3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
